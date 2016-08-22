2930ef46286408e728ca2dbbb7f94f8d0dfe6099
Jorge Gustavo Rocha <jgr@geomaster.pt>
2016-08-22 22:33:21 +0200

    Initial commit

---
+++ app/view/main/Map.js
@@ -0,0 +1,32 @@
+Ext.define("MyGeoExtApp.view.main.Map",{
+    // extend: "Ext.panel.Panel",
+    extend: "GeoExt.component.Map",
+    xtype: 'mappanel',
+    requires: [
+        "MyGeoExtApp.view.main.MapController",
+        "MyGeoExtApp.view.main.MapModel"
+    ],
+    controller: "main-map",
+    viewModel: {
+        type: "main-map"
+    },
+    // html: "Hello, World!!"
+    map: new ol.Map({
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.Stamen({
+                    layer: 'watercolor'
+                })
+            }),
+            new ol.layer.Tile({
+                source: new ol.source.Stamen({
+                    layer: 'terrain-labels'
+                })
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat( [-8.751278, 40.611368] ),
+            zoom: 12
+        })
+    })
+});
---
+++ app/view/main/MapModel.js
@@ -0,0 +1,8 @@
+Ext.define('MyGeoExtApp.view.main.MapModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.main-map',
+    data: {
+        name: 'MyGeoExtApp'
+    }
+
+});
